8f2c0bfa3ef839058586438fce281fea9f8ef3f0
kelz <kelsie.langston@yahoo.com>
2020-01-14 21:00:27 -0500

    add color change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 const axios = require('axios');
-var pdf = require('html-pdf');
+const conversion = require('phantom-html-to-pdf')();
 
 const fsWriteAsync = util.promisify(fs.writeFile);
 const fsReadAsync = util.promisify(fs.readFile);
@@ -30,7 +30,7 @@ async function promptUser() {
       {
         type: "input",
         name: "color",
-        message: "What's your favorit color?"
+        message: "What's your favorite color?"
       },
       {
         type: "input",
@@ -46,7 +46,7 @@ async function promptUser() {
 }
 
 
-function generateHTML(answers) {
+function generateHTML(answers, color) {
   return `
     <!DOCTYPE html>
     <html lang="en">
@@ -57,14 +57,78 @@ function generateHTML(answers) {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
         <title>Document</title>
         <link href="https://fonts.googleapis.com/css?family=Gelasio&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="./index.css">
+
+        <style>
+        body {
+          color: white;
+          background-color: lightgray;
+          font-family: 'Gelasio', serif;
+      }
+      .back {
+          position: relative;
+          height: 600px;
+          background-color: turquoise;
+      }
+      
+      .forefront {
+          position: absolute;
+          top: 100px;
+          width: 90%;
+          height: 600px;
+          left: 80px;
+          background-color: blue; 
+      }
+      
+      .avatar {
+          background-color: white;
+          position: absolute;
+          width: 300px;
+          height: 300px;
+          border-radius: 50%;
+          left: 40%;
+          top: 50px;
+          border: 3px solid yellow;
+      }
+      
+      .group {
+          position: relative;
+          top: 200px;
+          max-height: 350px;;
+      }
+      
+      .spacer {
+          position: relative;
+          top: 150px;
+          height: 400px;
+          width: 100%;
+          align-self: center;
+      }
+      
+      .block {
+          position: relative;
+          top: 250px;
+          height: 600px;
+          width: 100%;
+          background-color: turquoise;
+      }
+      
+      .info {
+          min-width: 300px;
+          background-color: blue;
+      }
+      
+      span {
+          color: white;
+      }
+        </style>
+
     </head>
     
     <body>
         <div class="back jumbotron jumbotron-fluid">
     
         </div>
-        <div class="forefront d-flex jumbotron justify-content-center">
+        <div class="forefront d-flex jumbotron justify-content-center" style="background-color: ${color};">
             <div class="group d-flex flex-column align-items-center">
                 <h1 class="display-4 mb-3">Hi!</h1>
                 <h1 class="mb-5">My name is ${answers.name}</h1>
@@ -79,21 +143,21 @@ function generateHTML(answers) {
         <div class="spacer d-flex flex-column">
             <p class="text-center h2 text-dark">${answers.bio}</p>
             <div class="d-flex flex-row justify-content-around">
-                <div class="info px-5 py-2 border border-dark text-center my-3">
+                <div class="info px-5 py-2 border border-dark text-center my-3" style="background-color: ${color};">
                     <p>Public Repositories</p>
                     <p>${answers.public_repos}</p>
                 </div>
-                <div class="info px-5 py-2 border border-dark text-center my-3">
+                <div class="info px-5 py-2 border border-dark text-center my-3" style="background-color: ${color};">
                     <p>Followers</p>
                     <p>${answers.followers}</p>
                 </div>
             </div>
             <div class="d-flex flex-row justify-content-around">
-                <div class="info px-5 py-2 border border-dark text-center my-3">
+                <div class="info px-5 py-2 border border-dark text-center my-3" style="background-color: ${color};">
                     <p>GitHub Stars</p>
                     <p>${answers.starred_url.length}</p>
                 </div>
-                <div class="info px-5 py-2 border border-dark text-center my-3">
+                <div class="info px-5 py-2 border border-dark text-center my-3" style="background-color: ${color};">
                     <p>Following</p>
                     <p>${answers.following}</p>
                 </div>
@@ -119,7 +183,7 @@ async function init() {
     //await the ajax call to github
     const { data } = await axios.get(`https://api.github.com/users/${github}`, githubHeader)
 
-    const htmlResults = generateHTML(data);
+    const htmlResults = generateHTML(data, color);
 
     //console.log(htmlResults)
 
@@ -132,12 +196,33 @@ async function init() {
       'orientation': 'portrait',
       "type": "pdf",             // allowed file types: png, jpeg, pdf
       "quality": "75",
-      "base": "file:///C:/Projects/BootCamp/Homework-9/developer-profile-generator/index.css"
+      "base": "./index.css"
     };
 
-    pdf.create(readHTML, options).toFile(`./pdfs/${name}'s_github_profile.pdf`, function (err, res) {
-      if (err) return console.log(err);
-      console.log(res); // { filename: '/pdfs/{user_profile}.pdf' }
+    // pdf.create(readHTML, options).toFile(`./pdfs/${name}'s_github_profile.pdf`, function (err, res) {
+    //   if (err) return console.log(err);
+    //   console.log(res); // { filename: '/pdfs/{user_profile}.pdf' }
+    // });
+
+    
+    conversion({ 
+      html: readHTML,
+      viewportSize: {
+        width: 600,
+        height: 600
+    },
+    format: {
+        quality: 100
+    } 
+    }, function(err, pdf) {
+    err ? console.log(err) : null;
+      var output = fs.createWriteStream(`./pdfs/${name}'s_github_profile.pdf`);
+      console.log(pdf.logs);
+      console.log(pdf.numberOfPages);
+        // since pdf.stream is a node.js stream you can use it
+        // to save the pdf to a file (like in this example) or to
+        // respond an http request.
+      pdf.stream.pipe(output);
     });
 
   } catch (error) {
